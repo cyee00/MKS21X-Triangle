556e545d0945e006cc6449463b520b5e4323f486
cyee00 <cyee00@stuy.edu>
2018-10-10 22:24:22 -0400

    commit

--- PointTriangle.java
+++ PointTriangle.java
@@ -16,10 +16,35 @@ public class PointTriangle {
     return y;
   }
   public double distanceTo(Point p1){
-    return Math.sqrt((Math.abs(p1.getX()-this.getX()))*(Math.abs(p1.getX()-this.getX())) + (Math.abs(p1.getY()-this.getY()))*(Math.abs(p1.getY()-this.getY())));
+    return Math.sqrt((Math.abs(p1.getX()-x))*(Math.abs(p1.getX()-x)) + (Math.abs(p1.getY()-y))*(Math.abs(p1.getY()-y)));
   }
-  public double distanceTo(Point p1, Point p2){
+  public double distance(Point p1, Point p2){
     return Math.sqrt((Math.abs(p2.getX()-p1.getX()))*(Math.abs(p2.getX()-p1.getX())) + (Math.abs(p2.getY()-p1.getY()))*(Math.abs(p2.getY()-p1.getY())));
   }
-}
+  public String toString() {
+    return "(" + x + "," + y + ")";
+  }
+  }
+  public class Triangle{
+    private double x1,x2,x3,y1,y2,y3;
+    public Triangle (Point p1, Point p2, Point p3){
+      x1=p1.getX();
+      x2=p2.getX();
+      x3=p3.getX();
+      y1=p1.getY();
+      y2=p2.getY();
+      y3=p3.getY();
+    }
+    public Triangle (double x1, double x2, double x3, double y1, double y2, double y3){
+      x1=x1;
+      x2=x2;
+      x3=x3;
+      y1=y1;
+      y2=y2;
+      y3=y3;
+    }
+    public double getPerimeter(){
+      distance
+    }
+  }
 }
