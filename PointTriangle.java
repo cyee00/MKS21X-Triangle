b287ca39728654bd049c80d5e4a311609003844e
cyee00 <cyee00@stuy.edu>
2018-10-10 23:44:20 -0400

    pt

--- PointTriangle.java
+++ PointTriangle.java
@@ -1,5 +1,5 @@
-public class PointTriangle {
-  public class Point{
+public class PointTriangle{
+  public class Point {
   private double x,y;
   public Point(double X, double Y){
     x=X;
@@ -24,27 +24,21 @@ public class PointTriangle {
   public String toString() {
     return "(" + x + "," + y + ")";
   }
+}
+public class Triangle {
+  private Point p1,p2,p3;
+  public Triangle (Point point1, Point point2, Point point3) {
+    Point p1 = point1;
+    Point p2 = point2;
+    Point p3 = point3;
   }
-  public class Triangle{
-    private double x1,x2,x3,y1,y2,y3;
-    public Triangle (Point p1, Point p2, Point p3){
-      x1=p1.getX();
-      x2=p2.getX();
-      x3=p3.getX();
-      y1=p1.getY();
-      y2=p2.getY();
-      y3=p3.getY();
-    }
-    public Triangle (double x1, double x2, double x3, double y1, double y2, double y3){
-      x1=x1;
-      x2=x2;
-      x3=x3;
-      y1=y1;
-      y2=y2;
-      y3=y3;
-    }
-    public double getPerimeter(){
-      distance
-    }
+  public Triangle (double x1, double x2, double x3, double y1, double y2, double y3) {
+    Point p1 = new Point(x1,y1);
+    Point p2 = new Point(x2,y2);
+    Point p3 = new Point(x3,y3);
   }
+  public double getPerimeter(){
+    return p1.distanceTo(p2) + p2.distanceTo(p3) + p3.distanceTo(p1);
+  }
+}
 }
