717657e6ae69410cb3067b7eddbe8d4bd486f1b8
cyee00 <cyee00@stuy.edu>
2018-10-11 10:11:03 -0400

    Driver

--- Driver.java
+++ Driver.java
@@ -1,87 +1,39 @@
 public class Driver{
-    public class Point {
-    private double x,y;
-    public Point(double X, double Y){
-      x=X;
-      y=Y;
-    }
-    public Point(Point p){
-      x= p.x;
-      y= p.y;
-    }
-    public double getX(){
-      return x;
-    }
-    public double getY(){
-      return y;
-    }
-    public double distanceTo(Point p1){
-      return Math.sqrt((Math.abs(p1.getX()-x))*(Math.abs(p1.getX()-x)) + (Math.abs(p1.getY()-y))*(Math.abs(p1.getY()-y)));
-    }
-    public double distance(Point p1, Point p2){
-      return Math.sqrt((Math.abs(p2.getX()-p1.getX()))*(Math.abs(p2.getX()-p1.getX())) + (Math.abs(p2.getY()-p1.getY()))*(Math.abs(p2.getY()-p1.getY())));
-    }
-    public String toString() {
-      return "(" + x + "," + y + ")";
-    }
-  }
-  public class Triangle {
-    private Point p1,p2,p3;
-    public Triangle (Point point1, Point point2, Point point3) {
-      Point p1 = point1;
-      Point p2 = point2;
-      Point p3 = point3;
-    }
-    public Triangle (double x1, double x2, double x3, double y1, double y2, double y3) {
-      Point p1 = new Point(x1,y1);
-      Point p2 = new Point(x2,y2);
-      Point p3 = new Point(x3,y3);
-    }
-    public double getPerimeter(){
-      return p1.distanceTo(p2) + p2.distanceTo(p3) + p3.distanceTo(p1);
-    }
-    public Point getVertex(int i){
-      Point ans;
-      if (i==0) {
-        ans = new Point(p1.getX(),p1.getY());
-      }
-      if (i==1) {
-        ans = new Point (p2.getX(),p2.getY());
-      }
-      if (i==2) {
-        ans = new Point(p3.getX(),p3.getY());
-      }
-      return ans;
-      }
-      public Point setVertex(int i, Point point){
-        Point ans;
-        if (i==0) {
-          p1 = Point(point.getX(),point.getY());
-        }
-        if (i==1) {
-          p2 = Point(point.getX(),point.getY());
-        }
-        if (i==2) {
-          p3 = Point(point.getX(),point.getY());
-        }
-    }
+
+  public static double distance(Point a, Point b){
+    return Math.sqrt(Math.pow(a.getX()-b.getX(),2)+Math.pow(a.getY()-b.getY(),2));
   }
 
-    public static void main(String[] args) {
-    Triangle abc = new Triangle(0, 0, 3, 0, 0, 4); // Initializes a 3-4-5 triangle
+  public static void main(String[]args){
+    Point p1 = new Point(1,1);
+    Point p2 = new Point(2,2);
+    System.out.println("Testing the distance 3 ways, should all be sqrt(2)");
+    System.out.println(  distance(p1,p2)); //static in the Driver class
+    System.out.println(  Point.distance(p1,p2)); //static in the Point class
+    System.out.println(  p1.distanceTo(p2)); //non-static in the Point class.
 
-    System.out.println(abc); //should print triangle in format specified on website
-    System.out.println("\nPerimeter: " + abc.getPerimeter()); //should be 12.0
+    System.out.println("\nTesting 2 identical traingles, perimeter is 2+sqrt(2)");
+    Triangle t1 = new Triangle(0,0,0,1,1,0);
+    Triangle t2 = new Triangle(new Point(0,0),new Point(0,1),new Point(1,0));
+    System.out.println( t1 );
+    System.out.println( t1.getPerimeter() );
+    System.out.println( t2 );
+    System.out.println( t2.getPerimeter() );
 
-    System.out.println("\nVertices: " + abc.getVertex(0) + ", " + abc.getVertex(1) + ", " + abc.getVertex(1)); //should print individual vertices
+    System.out.println("\nTesting a 3/4/5 triangle");
+    t1 = new Triangle(-1,-1, 2,3, 2,-1);
+    System.out.println( t1 );
+    System.out.println( t1.getPerimeter() );
 
-    abc.setVertex(0, new Point(1, 1)); //sets point at the origin to (1,1)
+    System.out.println("\nTesting getVertex in the 3/4/5 triangle");
+    System.out.println( t1.getVertex(0) );
+    System.out.println( t1.getVertex(1) );
+    System.out.println( t1.getVertex(2) );
 
-    System.out.println("\nVertex A moved to " + abc.getVertex(2)); //should show new value as (1,1)
-
-    System.out.println("New Perimeter: " + abc.getPerimeter()); //should be approximately 10.39 (truncated)
+    System.out.println("\nTesting setPoint in the 3/4/5 triangle");
+    t1.setVertex(0,new Point(9,9));
+    t1.setVertex(1,new Point(-9,9));
+    t1.setVertex(2,new Point(9,-9));
+    System.out.println( t1 );
   }
-}
-
-
-// Driver credits to Tejas Narayan.
+ }
